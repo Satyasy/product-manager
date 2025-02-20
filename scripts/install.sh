e337f666a6f760354fc56cbad6a1ecceafd43321
Satyasy <satyavpandega@gmail.com>
2025-02-20 13:24:22 +0700

    add buildspec.yml

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,6 @@ echo "Installing dependencies..."
 cd /var/www/laravel-cicd
 composer install --no-dev --prefer-dist --optimize-autoloader
 php artisan migrate --force
+php artisan config:clear
+php artisan cache:clear
+php artisan config:cache
